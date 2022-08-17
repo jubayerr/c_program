488bc66a0ef075abd50663a1102e8e3c3d668e1f
jubayerr <showkatjubayer@gmail.com>
2022-08-17 11:25:08 +0600

    sum

---
+++ var.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// data type, input output (cpbook, chapter 2)
+
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    a = 50;
+    b = 60;
+
+    sum = a + b;
+
+    printf("Sum is %d\n", sum);
+
+    return 0;
+}
