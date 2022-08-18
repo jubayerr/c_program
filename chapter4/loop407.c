39eca9f9071b5e8fcc2afe6f1a0479da106461db
jubayerr <showkatjubayer@gmail.com>
2022-08-18 20:09:33 +0600

    loop

---
+++ chapter4/loop407.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// multiplication table, for loop 4.7 (cpbook)
+
+int main()
+{
+    int n = 5;
+    int i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+    return 0;
+}
