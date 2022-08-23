0603aff47917fc255837374987716aa315cebb75
jubayerr <showkatjubayer@gmail.com>
2022-08-23 10:29:36 +0600

    string

---
+++ chapter9/string01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// string 9.1 (cpbook)
+
+int main()
+{
+    char country_name[] = {'B', 'a', 'n', 'g', 'l', 'a', 'd', 'e', 's', 'h', '\0'};
+
+    printf("%s\n", country_name);
+
+    return 0;
+}
