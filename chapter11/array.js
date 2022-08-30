e92d5417463e091aec8a7bf5372749221b9e9d10
jubayerr <showkatjubayer@gmail.com>
2022-08-30 16:24:38 +0600

    c

--- chapter11/array.js
+++
@@ -1,8 +0,0 @@
-// 1 dimensional array in JavaScript
-
-var countryList = ["Bangladesh", "India", "Pakistan", "Sri Lanka", "Nepal", "Bhutan", "Maldives"]
-
-for (let i = 0; i < countryList.length; i++) {
-    const element = countryList[i];
-    console.log(element);
-}
